fb06f9f6d7721acdfa47d03e7440f0b9f257947a
Heeho Lim <heeho1998@gmail.com>
2023-07-14 20:07:42 +0900

    make server-app

---
+++ server-app/src/main/java/com/serverapp/CreateTodoRequest.java
@@ -0,0 +1,14 @@
+package com.serverapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTodoRequest {
+    private String todoName;
+}
